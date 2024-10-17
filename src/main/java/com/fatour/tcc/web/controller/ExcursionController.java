2a47bf9acb05a0927821738c356e70d3d335c555
VitorSantana113 <vitor@t2s.com.br>
2024-10-17 00:57:45 -0300

    Atualização de CORS no arquivo "ExcursionController.java"

--- src/main/java/com/fatour/tcc/web/controller/ExcursionController.java
+++ src/main/java/com/fatour/tcc/web/controller/ExcursionController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/excursions")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class ExcursionController {
 
     private ExcursionService excursionService;
