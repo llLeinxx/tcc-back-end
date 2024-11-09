a2d8bd5247786218abb933b72403e55ff94a3009
Victor Iglesias <victoraaiglesias@gmail.com>
2024-11-09 00:27:18 -0300

    fix repository

--- src/main/java/com/fatour/tcc/reporitory/PaymentRepository.java
+++ src/main/java/com/fatour/tcc/reporitory/PaymentRepository.java
@@ -4,9 +4,11 @@ import com.fatour.tcc.entity.Excursion;
 import com.fatour.tcc.entity.Payment;
 import com.fatour.tcc.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByUsuario(Usuario usuario);
 }
--- src/main/java/com/fatour/tcc/reporitory/SeatRepository.java
+++ src/main/java/com/fatour/tcc/reporitory/SeatRepository.java
@@ -6,9 +6,12 @@ import com.fatour.tcc.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+@Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
     boolean existsByExcursionAndSeatNumber(Excursion excursion, String numberSeat);
 
